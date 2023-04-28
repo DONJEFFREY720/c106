a2755575b01b9c0be46d6277468dfb09183b4b52
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-04-28 20:04:16 +0530

    plese be final

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ function startClassification(){
      ({
           audio : true
      });
-     classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/74hgqAgD1.JSON",modelReady);
+     classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/uGkyOcaJ8/model.json",modelReady);
 }
 
 
-/*function modelReady(){
+function modelReady(){
   classifier.classify(gotResults);
 }
 var dog = 0;
 var cat = 0;
+var cow = 0;
+var cokoo =0;
 
 function gotResults(error, results) {
   if (error) {
@@ -30,14 +32,20 @@ function gotResults(error, results) {
 
     img = document.getElementById('animal_image');
 
-    if (results[0].label == "Barking") {
-      img.src = 'bark.gif';
+    if (results[0].label == "DOG") {
+      img.src = 'https://shravaripatil.github.io/Sound_controlled_animals/bark.gif';
       dog = dog+1;
-    } else if (results[0].label == "Meowing") {
-      img.src = 'meow.gif';
+    } else if (results[0].label == "CAT") {
+      img.src = 'https://shravaripatil.github.io/Sound_controlled_animals/meow.gif';
       cat = cat + 1;
+    } else if (results[0].label == "COW") {
+      img.src = 'https://gifdb.com/images/high/cartoon-cow-walk-61a1lckxexg0nc6f.gif';
+      cow = cow + 1;
+    }  else if (results[0].label == "COKOO") {
+      img.src = 'https://i.gifer.com/origin/cc/cc701c835cbd2b4a1eaffd22b1596a79_w200.gif';
+      cow = cokoo + 1;
     } else{
-      img.src = 'listen.gif';
+      img.src = 'https://shravaripatil.github.io/Sound_controlled_animals/listen.gif';
     }
   }
-}*/
+}
